be2590d5eaa7551297b1d6cb9ad0cb199de2e6a8
Clazariuk1 <Clazariuk@gmail.com>
2023-12-12 16:41:11 -0800

    counter value at 1 to start

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ const displayValue = document.getElementById("display-value")
 let counterValue = document.getElementById("counter-value")
 const incrementBtn = document.getElementById("increment-button")
 const decrementBtn = document.getElementById("decrement-button")
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+counterValue.value = 1
 
 function createCounter() {
   let count = 0;
-
   return function (action) {
     if (action === "add") {
       count += parseInt(counterValue.value)
